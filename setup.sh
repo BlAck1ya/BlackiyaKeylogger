a72285f71f76d7e4e45cf5b23c212b49dfa7924d
Blackiya <swamijii312@gmail.com>
2026-06-09 09:52:33 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/BlAck1ya/BlackiyaKeylogger.git
-cd BlackiyaKeylogger
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python3 BlackiyaKeylogger.py
